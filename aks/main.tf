826fa09921d050bac17a860b48860082e917a9fb
Shivam Singhal <ssinghal@topcon.com>
2024-02-01 01:55:55 +0200

    change vm size

--- aks/main.tf
+++ aks/main.tf
@@ -33,7 +33,7 @@ resource "azurerm_kubernetes_cluster" "aks-gh-tf" {
   default_node_pool {
     name       = "default"
     node_count = var.node_count
-    vm_size    = "Standard_B2pts_v2"
+    vm_size    = "Standard_D2as_v4"
     
   }
 
